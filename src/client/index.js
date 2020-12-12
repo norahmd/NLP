8fe7f1f7de534c4d0a2dccbdd9541c905df9d6f0
norahmd <435200095@student.ksu.edu.sa>
2020-12-12 17:37:10 +0300

    add modes

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,6 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import { checkForName } from './js/nameChecker.js'
+import { handleSubmit } from './js/formHandler.js'
+
+console.log(checkForName);
 
 alert('I am here')
---
+++ webpack.dev.js
@@ -0,0 +1,39 @@
+const path = require("path")
+const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development',
+    devtool: 'source-map',
+    entry: './src/client/index.js',
+    // devServer: {
+    //     contentBase: path.join(__dirname, 'dist'),
+    //     compress: true,
+    //     port: 9000
+    //   },
+    module: {
+        rules: [
+                {
+            test: '/\.(js)$/',
+            exclude: /node_modules/,
+            use: ['babel-loader']
+                }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/client/views/index.html',
+            filename: './index.html'
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+    })  
+    ]    
+}
